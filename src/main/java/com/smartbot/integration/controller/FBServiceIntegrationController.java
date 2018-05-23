3d57aad0f69a75f660ce2f0ba571c92b6144f167
Raju KC <raju.kc@igctech.com.np>
2018-05-23 16:21:04 +0545

    -minor fixes.

--- src/main/java/com/smartbot/integration/controller/FBServiceIntegrationController.java
+++ src/main/java/com/smartbot/integration/controller/FBServiceIntegrationController.java
@@ -1,7 +1,5 @@
 package com.smartbot.integration.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -18,50 +16,39 @@ import com.github.messenger4j.receive.MessengerReceiveClient;
 import com.smartbot.integration.service.FBMessageService;
 
 @RestController
-@RequestMapping("/facebookwebhook") 
+@RequestMapping("/facebookwebhook")
 public class FBServiceIntegrationController {
 
-	private static final Logger logger = LoggerFactory.getLogger(FBServiceIntegrationController.class);
+  private final MessengerReceiveClient receiveClient;
 
-	private final MessengerReceiveClient receiveClient;
+  @Autowired
+  public FBServiceIntegrationController(@Value("${messenger4j.appSecret}") final String appSecret,
+      @Value("${messenger4j.verifyToken}") final String verifyToken,
+      final FBMessageService messageService) {
+    this.receiveClient = MessengerPlatform.newReceiveClientBuilder(appSecret, verifyToken)
+        .onTextMessageEvent(messageService.newTextMessageEventHandler()).build();
+  }
 
-	@Autowired
-	public FBServiceIntegrationController(@Value("${messenger4j.appSecret}") final String appSecret,
-			@Value("${messenger4j.verifyToken}") final String verifyToken, final FBMessageService messageService) {
+  @RequestMapping(method = RequestMethod.GET)
+  public ResponseEntity<String> verifyWebhook(@RequestParam("hub.mode") final String mode,
+      @RequestParam("hub.verify_token") final String verifyToken,
+      @RequestParam("hub.challenge") final String challenge) {
+    try {
+      return ResponseEntity.ok(this.receiveClient.verifyWebhook(mode, verifyToken, challenge));
+    } catch (MessengerVerificationException e) {
+      return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+    }
+  }
 
-		logger.debug("Initializing MessengerReceiveClient - appSecret: {} | verifyToken: {}", appSecret, verifyToken);
-		this.receiveClient = MessengerPlatform.newReceiveClientBuilder(appSecret, verifyToken)
-				.onTextMessageEvent(messageService.newTextMessageEventHandler()).build();
-	}
-
-	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<String> verifyWebhook(@RequestParam("hub.mode") final String mode,
-			@RequestParam("hub.verify_token") final String verifyToken,
-			@RequestParam("hub.challenge") final String challenge) {
-
-		logger.debug("Received Webhook verification request - mode: {} | verifyToken: {} | challenge: {}", mode,
-				verifyToken, challenge);
-		try {
-			return ResponseEntity.ok(this.receiveClient.verifyWebhook(mode, verifyToken, challenge));
-		} catch (MessengerVerificationException e) {
-			logger.warn("Webhook verification failed: {}", e.getMessage());
-			return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
-		}
-	}
-
-	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> handleCallback(@RequestBody final String payload,
-			@RequestHeader("X-Hub-Signature") final String signature, String recpientId) {
-
-		logger.debug("Received Messenger Platform callback - payload: {} | signature: {}", payload, signature);
-		try {
-		    this.receiveClient.processCallbackPayload(payload, signature);
-			logger.debug("Processed callback payload successfully");
-			return ResponseEntity.status(HttpStatus.OK).build();
-		} catch (MessengerVerificationException e) {
-			logger.warn("Processing of callback payload failed: {}", e.getMessage());
-			return ResponseEntity.status(HttpStatus.OK).build();
-		}
-	}
+  @RequestMapping(method = RequestMethod.POST)
+  public ResponseEntity<Void> handleCallback(@RequestBody final String payload,
+      @RequestHeader("X-Hub-Signature") final String signature, String recpientId) {
+    try {
+      this.receiveClient.processCallbackPayload(payload, signature);
+      return ResponseEntity.status(HttpStatus.OK).build();
+    } catch (MessengerVerificationException e) {
+      return ResponseEntity.status(HttpStatus.OK).build();
+    }
+  }
 
 }
