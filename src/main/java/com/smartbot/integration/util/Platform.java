7b7578d97a23e92cc6ebcf2ac54b145efc3148a0
Raju KC <raju.kc@igctech.com.np>
2018-05-23 11:28:47 +0545

    -Change to interface.

--- src/main/java/com/smartbot/integration/util/Platform.java
+++ src/main/java/com/smartbot/integration/util/Platform.java
@@ -1,8 +1,8 @@
 package com.smartbot.integration.util;
 
-public class Platform {
-  final public static String FB = "Facebook Messenger";
-  final public static String SLACK = "Slack";
-  final public static String SKYPE = "Skype";
-  final public static String VIBER = "Viber";
+public interface Platform {
+  String FB = "Facebook Messenger";
+  String SLACK = "Slack";
+  String SKYPE = "Skype";
+  String VIBER = "Viber";
 }
