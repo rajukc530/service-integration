3b56a6526f390bc62dcf4642e006f9d464345508
Raju KC <raju.kc@igctech.com.np>
2018-05-23 10:57:37 +0545

    -Added Platform constants.

---
+++ src/main/java/com/smartbot/integration/util/Platform.java
@@ -0,0 +1,8 @@
+package com.smartbot.integration.util;
+
+public class Platform {
+  final public static String FB = "Facebook Messenger";
+  final public static String SLACK = "Slack";
+  final public static String SKYPE = "Skype";
+  final public static String VIBER = "Viber";
+}
