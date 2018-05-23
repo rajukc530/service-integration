8a1f3d9adfdb4586171a602ab892e803ab31df6e
Raju KC <raju.kc@igctech.com.np>
2018-05-23 15:35:10 +0545

    --added new fb page.

--- src/main/java/com/smartbot/integration/util/Constants.java
+++ src/main/java/com/smartbot/integration/util/Constants.java
@@ -7,7 +7,7 @@ public interface Constants {
   HashMap<String, String> fbPageMapper = new HashMap<String, String>() {
     private static final long serialVersionUID = -1644315397076096817L;
     {
-      put("pageid","pageToken");
+      put("247652549116924","EAAaISLtfMnUBAJjQz01udOEzM5HHZAKpgXumFjCQ7eHqm2r00IjBudyySKWCP229IiCx3oAyM5jfiIVymCZBDzfKhMrDk54pFdZBGBQpBKdENVgVIZCsUkIipd1iODhh0ioxZAco5ZC5cMVJo0OGRVCUlniDAGqguKVyaHyhN1egZDZD");   // Raju Smart Bot Page 1
       put("206469290133580", "EAAaISLtfMnUBABXld1fpZBfAynghOfIhZAP9PuD7W9FP99OTHZCdIkGotO7fEp102Ymh9BX3aw88VuOUJ8F8RTSn96aCVgIdZBHChZA6W2zNZB6ebapkVNZAFawm7ddSZC5osIA2uFYUaOdQ5LsCo2pTpS8XZCdUtxzSZBpamzMx2zaAZDZD");  //Raju-Smart-Bot page
       put("619310598419669", "EAAaISLtfMnUBAGtnWz9Am00JZAmn2jPH2qFdeTKE08JQvZAER4BkI6Orc6Ul92ynqtZB0pN6awZCFppPMnckZCYRZAUY6BsZB3G2c4ihRutZALrGPB6xusAawZBnOOfCzGIk674ZCgZAhwdj64riqczW5kjBRb9dw9DcZBCJw8DDGPsvVgZDZD");         //Naulo-post page
     }
